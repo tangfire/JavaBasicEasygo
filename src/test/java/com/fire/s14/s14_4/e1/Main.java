6357d5aa3e3cccbcad170232f1d3595b078a8c5d
tangfire <52628216@qq.com>
2024-12-17 12:31:30 +0800

    daemon thread

--- src/test/java/com/fire/s14/s14_4/e1/Main.java
+++ src/test/java/com/fire/s14/s14_4/e1/Main.java
@@ -1,6 +1,16 @@
 package com.fire.s14.s14_4.e1;
 
 // 中断线程
+
+/**
+ * 如果线程需要执行一个长时间任务，就可能需要能中断线程。中断线程就是其他线程给该线程发一个信号，该线程收到信号后结束执行run()方法，使得自身线程能立刻结束运行。
+ *
+ * 我们举个栗子：假设从网络下载一个100M的文件，如果网速很慢，用户等得不耐烦，就可能在下载过程中点“取消”，这时，程序就需要中断下载线程的执行。
+ *
+ * 中断一个线程非常简单，只需要在其他线程中对目标线程调用interrupt()方法，目标线程需要反复检测自身状态是否是interrupted状态，如果是，就立刻结束运行。
+ *
+ * 我们还是看示例代码：
+ */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Thread t = new MyThread();
@@ -21,3 +31,10 @@ class MyThread extends Thread {
         }
     }
 }
+
+/**
+ * 仔细看上述代码，main线程通过调用t.interrupt()方法中断t线程，
+ * 但是要注意，interrupt()方法仅仅向t线程发出了“中断请求”，至于t线程是否能立刻响应，
+ * 要看具体代码。而t线程的while循环会检测isInterrupted()，
+ * 所以上述代码能正确响应interrupt()请求，使得自身立刻结束运行run()方法。
+ */
---
+++ src/test/java/com/fire/s14/s14_4/e2/Main.java
@@ -0,0 +1,54 @@
+package com.fire.s14.s14_4.e2;
+
+// 中断线程
+
+/**
+ * 如果线程处于等待状态，例如，t.join()会让main线程进入等待状态，此时，如果对main线程调用interrupt()，join()方法会立刻抛出InterruptedException，
+ * 因此，目标线程只要捕获到join()方法抛出的InterruptedException，就说明有其他线程对其调用了interrupt()方法，通常情况下该线程应该立刻结束运行。
+ *
+ * 我们来看下面的示例代码：
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new MyThread();
+        t.start();
+        Thread.sleep(1000);
+        t.interrupt(); // 中断t线程
+        t.join(); // 等待t线程结束
+        System.out.println("end");
+    }
+}
+
+class MyThread extends Thread {
+    public void run() {
+        Thread hello = new HelloThread();
+        hello.start(); // 启动hello线程
+        try {
+            hello.join(); // 等待hello线程结束
+        } catch (InterruptedException e) {
+            System.out.println("interrupted!");
+        }
+        hello.interrupt();
+    }
+}
+
+class HelloThread extends Thread {
+    public void run() {
+        int n = 0;
+        while (!isInterrupted()) {
+            n++;
+            System.out.println(n + " hello!");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+}
+
+/**
+ * main线程通过调用t.interrupt()从而通知t线程中断，而此时t线程正位于hello.join()的等待中，此方法会立刻结束等待并抛出InterruptedException。
+ * 由于我们在t线程中捕获了InterruptedException，
+ * 因此，就可以准备结束该线程。在t线程结束前，对hello线程也进行了interrupt()调用通知其中断。如果去掉这一行代码，可以发现hello线程仍然会继续运行，且JVM不会退出。
+ */
